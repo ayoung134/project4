96e037e5fc6fc634e017fe31337ace69e8beb0e3
ayoung134 <ayou134@lsu.edu>
2026-04-27 17:31:05 -0500

    started project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // JS scripts placed here
+
+// navbar
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu')
+
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+});
+// end navbar
